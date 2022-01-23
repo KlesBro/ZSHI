ac3ee34f26178dae1d66ec5d45ee6702c942da06
KlesBro <85430054+KlesBro@users.noreply.github.com>
2022-01-23 14:22:46 +0300

    Add files via upload

---
+++ ZSHI.sh
@@ -0,0 +1,13 @@
+clear
+echo --- ZSHI ---
+echo Installing zsh...
+sudo apt install zsh
+echo Done!
+echo Setting up .zshrc... 
+cp ./CFG/1/.zshrc ~/
+echo Done!
+echo Setting up .zshrc...
+cp ./CFG/2/.zshrc ~/
+echo All Done!
+
+
