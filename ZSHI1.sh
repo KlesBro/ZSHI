aa6f2d3ae774cb022dda43dfad84c520b71faee8
KlesBro <85430054+KlesBro@users.noreply.github.com>
2022-01-25 09:47:10 +0300

    Add files via upload

---
+++ ZSHI1.sh
@@ -0,0 +1,15 @@
+clear
+echo " "
+echo "███████╗███████╗██╗  ██╗██╗██╗
+╚══███╔╝██╔════╝██║  ██║██║██║
+  ███╔╝ ███████╗███████║██║██║
+ ███╔╝  ╚════██║██╔══██║██║╚═╝
+███████╗███████║██║  ██║██║██╗
+╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝"
+echo ------------------
+echo Installing zsh...
+sudo apt install zsh
+echo Done!
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+                              
---
+++ ZSHI2.sh
@@ -0,0 +1,23 @@
+clear
+echo " "
+echo "███████╗███████╗██╗  ██╗██╗██╗
+╚══███╔╝██╔════╝██║  ██║██║██║
+  ███╔╝ ███████╗███████║██║██║
+ ███╔╝  ╚════██║██╔══██║██║╚═╝
+███████╗███████║██║  ██║██║██╗
+╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝"
+echo ------------------
+echo Downloading plugins for zsh...
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo Done!
+echo Setting up .zshrc...
+cp ./CFG/1/.zshrc ~/ #high
+echo Done!
+echo Downloading theme for zsh...
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo Done!
+echo Setting up .zshrc...
+cp ./CFG/2/.zshrc ~/ #p10k
+echo All done!
+zsh
+
