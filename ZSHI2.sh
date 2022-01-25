e375afd3c9a71eaa206899191ce7f69c57b1f1ca
KlesBro <85430054+KlesBro@users.noreply.github.com>
2022-01-25 12:14:24 +0300

    Add files via upload

--- ZSHI2.sh
+++ ZSHI2.sh
@@ -8,16 +8,15 @@ echo "███████╗███████╗██╗  ██╗██
 ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝"
 echo ------------------
 echo Downloading plugins for zsh...
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 echo Done!
 echo Setting up .zshrc...
 cp ./CFG/1/.zshrc ~/ #high
 echo Done!
 echo Downloading theme for zsh...
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 echo Done!
 echo Setting up .zshrc...
 cp ./CFG/2/.zshrc ~/ #p10k
 echo All done!
-zsh
 
